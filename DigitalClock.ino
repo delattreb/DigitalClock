0e7e8d9626d60e821c4729465496ea511b62bc27
DELATTRE, BRUNO <bruno.delattre@econocom.com>
2017-11-06 22:01:40 +0100

    Ajoutez des fichiers projet.

---
+++ DigitalClock.ino
@@ -0,0 +1,120 @@
+//
+// Created by Bruno on 6/11/2017.
+//
+
+#include <Wire.h>
+#include "var.h"
+#include "libDS3231.h"
+#include "libAdafruitNeoPixel.h"
+
+// Var
+Adafruit_NeoPixel ringled = Adafruit_NeoPixel(NBPIXELS_RINGLED, PIN_RINGLED, NEO_GRB + NEO_KHZ800);
+
+
+int pix_hour, pix_minute, pix_seconde, pix_dixseconde, lastpix_dixseconde;
+unsigned long startmillis;
+
+libDS3231 rtc;
+RtcDateTime now;
+int color = 0, mode = 0, light = 55;
+boolean bfm_mode = false, bfm_color = false, bfm_light = false;
+
+// Color
+int colorTable[4][4][3] = { { { 0,0,255 },{ 0,0,255 },{ 220,0,255 },{ 255,40,40 } },
+{ { 255,45,0 },{ 255,45,0 },{ 255,200,0 },{ 40,255,40 } },
+{ { 0,255,45 },{ 0,255,45 },{ 0,200,200 },{ 150,150,0 } },
+{ { 255, 0, 0 },{ 255,0,0 },{ 255,45,0 },{ 255,0,255 } } };
+uint32_t color_hour;
+uint32_t color_minute;
+uint32_t color_seconde;
+uint32_t color_dixseconde;
+
+//
+// setup
+//
+void setup() {
+#ifdef DEBUG
+	Serial.begin(BAUDS_RATE);
+	while (!Serial) {
+		;
+	}
+#endif 
+
+	pinMode(PIN_MODE, INPUT);
+	pinMode(PIN_COLOR, INPUT);
+	pinMode(PIN_LIGHT, INPUT);
+
+	setColor(color);
+	rtc.init(false);
+	startmillis = millis();
+
+	// Init NeoPixel
+	ringled.setBrightness(light);
+	ringled.begin();
+	ringled.show();
+}
+
+//
+// loop
+//
+void loop() {
+	readInput();
+	setColor(color);
+	ringled.setBrightness(light);
+
+	//Get time
+	now = rtc.getDateTime();
+	pix_hour = now.Hour()+ (now.Minute() / 5);
+	ringled.setPixelColor(pix_hour, color_hour);
+
+	ringled.show();
+}
+
+
+//
+// readInput
+//
+void readInput()
+{
+	//Load input mode
+	if (digitalRead(PIN_MODE) == HIGH && !bfm_mode) {
+		bfm_mode = true;
+		mode += 1;
+		if (mode > 2)
+			mode = 0;
+	}
+	if (digitalRead(PIN_MODE) == LOW)
+		bfm_mode = false;
+
+	//Load input color 
+	if (digitalRead(PIN_COLOR) == HIGH && !bfm_color) {
+		bfm_color = true;
+		color += 1;
+		if (color > 3)
+			color = 0;
+	}
+	if (digitalRead(PIN_COLOR) == LOW)
+		bfm_color = false;
+
+	//Load input light 
+	if (digitalRead(PIN_LIGHT) == HIGH && !bfm_light) {
+		bfm_light = true;
+		light += 20;
+		if (light > 255)
+			light = 10;
+	}
+	if (digitalRead(PIN_LIGHT) == LOW)
+		bfm_light = false;
+}
+
+//
+// setColor
+//
+void setColor(int col)
+{
+	color_hour = ringled.Color(colorTable[col][0][0], colorTable[col][0][1], colorTable[col][0][2], GAMMA);
+	color_minute = ringled.Color(colorTable[col][1][0], colorTable[col][1][1], colorTable[col][1][2], GAMMA);
+	color_seconde = ringled.Color(colorTable[col][2][0], colorTable[col][2][1], colorTable[col][2][2], GAMMA);
+	color_dixseconde = ringled.Color(colorTable[col][3][0], colorTable[col][3][1], colorTable[col][3][2], GAMMA);
+}
+
---
+++ var.h
@@ -0,0 +1,17 @@
+
+
+#define DEBUG
+#define INFO
+#define BAUDS_RATE 115200
+
+#define NBREMANANCE	4
+
+#define PIN_RINGLED 4
+#define NBPIXELS_RINGLED 60
+
+#define GAMMA 0
+
+// PIN INPUT
+#define PIN_MODE 7
+#define PIN_COLOR 8
+#define PIN_LIGHT 9
